8f9683190942660e64141532e339a6d9f1f018e0
Christophe Desplanches <kcdesplanches@gmail.com>
2022-01-15 16:07:40 -0500

    Nothing to add.

--- graphql/app-config.js
+++ graphql/app-config.js
@@ -2,6 +2,10 @@ const PORT = 5000;
 const environment = {
     development: {
         serverURL: `http://localhost:${PORT}/`,
+        dbString: 'mongodb://localhost:27017/gql?retryWrites=true&w=majority'
+    },
+    debug: {
+        serverURL: `http://localhost:${PORT}/`,
         dbString: 'mongodb://mongo:27017/gql?retryWrites=true&w=majority'
     },
     production: {
